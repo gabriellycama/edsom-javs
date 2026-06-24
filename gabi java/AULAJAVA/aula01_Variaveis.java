9d2ce68bd20a8f128fe93d9aa421132d9f44da7b
gabriellycama <gabrielly.camargo.pereira@escola.pr.gov.br>
2026-06-24 09:53:21 -0300

    Add files via upload

---
+++ gabi java/AULAJAVA/aula01_Variaveis.java
@@ -0,0 +1,21 @@
+package AULAJAVA;
+
+public class aula01_Variaveis{
+ 
+    public static void main(String[] args){
+    int idade = 17;
+    double altura = 1.75;
+    boolean matriculado = true;
+    String nome = "gabi";
+    
+
+    System.out.println("nome:" + nome);
+    System.out.println("idade:" + idade + "anos");
+    System.out.println("altura:" + altura + "m");
+    System.out.println(" aluno matriculado?:" + matriculado);
+
+
+}
+
+
+}
